e762e82b71946a337545e6b1d244b991b48d6d08
MasterEnderman <xxmr.endermanxx@gmail.com>
2024-11-12 01:46:29 +0100

    added the option to force lock entries using the "force_locked" key and setting it true

--- src/main/java/io/enderdev/patchoulibooks/mixins/patchouli/BookEntryMixin.java
+++ src/main/java/io/enderdev/patchoulibooks/mixins/patchouli/BookEntryMixin.java
@@ -1,10 +1,13 @@
 package io.enderdev.patchoulibooks.mixins.patchouli;
 
+import com.google.gson.annotations.SerializedName;
 import io.enderdev.patchoulibooks.Tags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import vazkii.patchouli.client.book.BookEntry;
 import vazkii.patchouli.client.book.EntryDisplayState;
@@ -16,6 +19,13 @@ public class BookEntryMixin {
     @Shadow
     Book book;
 
+    @Shadow
+    boolean locked;
+
+    @Unique
+    @SerializedName("force_locked")
+    public boolean force_locked;
+
     /**
      * Mark all my entries as read by default instead of having to set this for each entry
      */
@@ -24,6 +34,21 @@ public class BookEntryMixin {
         if (book.owner.getModId().equals(Tags.MOD_ID)) {
             cir.setReturnValue(EntryDisplayState.NEUTRAL);
             cir.cancel();
+        }
+    }
+
+    @Inject(method = "isLocked", at = @At("HEAD"), cancellable = true)
+    private void isLocked(CallbackInfoReturnable<Boolean> cir) {
+        if (force_locked) {
+            cir.setReturnValue(true);
+            cir.cancel();
+        }
+    }
+
+    @Inject(method = "updateLockStatus", at = @At("HEAD"))
+    private void updateLockStatus(CallbackInfo ci) {
+        if (force_locked) {
+            locked = true;
         }
     }
 }
