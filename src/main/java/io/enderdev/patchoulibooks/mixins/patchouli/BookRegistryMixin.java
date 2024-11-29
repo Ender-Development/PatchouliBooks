758a51d647791d4edb299b0ae08194f97b582816
MasterEnderman <xxmr.endermanxx@gmail.com>
2024-11-29 15:59:38 +0100

    fix crash when external book doesn't use my BookExtension

--- src/main/java/io/enderdev/patchoulibooks/mixins/patchouli/BookRegistryMixin.java
+++ src/main/java/io/enderdev/patchoulibooks/mixins/patchouli/BookRegistryMixin.java
@@ -1,7 +1,6 @@
 package io.enderdev.patchoulibooks.mixins.patchouli;
 
 import com.google.gson.Gson;
-import com.google.gson.annotations.SerializedName;
 import io.enderdev.patchoulibooks.PatchouliBooks;
 import io.enderdev.patchoulibooks.Tags;
 import io.enderdev.patchoulibooks.config.ConfigMain;
@@ -19,10 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import vazkii.patchouli.common.book.Book;
 import vazkii.patchouli.common.book.BookRegistry;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
@@ -37,30 +33,55 @@ public class BookRegistryMixin {
     public Gson gson;
 
     @Inject(method = "loadBook", at = @At("HEAD"), cancellable = true)
-    private void loadBook(ModContainer mod, ResourceLocation res, InputStream stream, boolean external, CallbackInfo ci) {
+    private void loadBook(ModContainer mod, ResourceLocation res, InputStream stream, boolean external, CallbackInfo ci) throws IOException {
         PatchouliBooks.LOGGER.debug("Loading {}: [Mod]<{}> || [Book]<{}>", external ? "EXTERNAL" : "INTERNAL", mod.getModId(), res.getPath());
+        // return if the book is added by another mod
         if (!mod.getModId().equals(Tags.MOD_ID) && !external) {
             return;
         }
+        // return if the required mod for one of my books isn't loaded
         if (!Loader.isModLoaded(res.getPath()) && !ConfigMain.ConfigDebug.enableDebug && !external) {
             PatchouliBooks.LOGGER.debug("Cancelling: [Book]<{}>, because the mod is not present.", res.getPath());
             ci.cancel();
             return;
         }
-        Reader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
-        BookExtension book = gson.fromJson(reader, BookExtension.class);
+
+        // No return after this point as it consumes the InputStream
+        // The book.json file should be small enough to load it in memory
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        patchouliBooks$copyStream(stream, buffer);
+        byte[] data = buffer.toByteArray();
+
+        // duplicate the Reader so I can read it multiple times
+        Reader reader1 = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(data), StandardCharsets.UTF_8));
+        Reader reader2 = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(data), StandardCharsets.UTF_8));
+
+        BookExtension book = gson.fromJson(reader1, BookExtension.class);
         if (book == null) {
             PatchouliBooks.LOGGER.error("Failed to load [Book]<{}> from [Mod]<{}>", res, mod.getModId());
-            PatchouliBooks.LOGGER.error("Returning to normal loading...");
+            ci.cancel();
             return;
         }
+        // implement default book building
         if (!book.bookPlus && !mod.getModId().equals(Tags.MOD_ID)) {
-            return;
+            Book pbook = this.gson.fromJson(reader2, Book.class);
+            this.books.put(res, pbook);
+            pbook.build(mod, res, external);
         }
-        if (!book.bookDisabled) {
+        // build my BookExtension
+        if (!book.bookDisabled && !books.containsKey(res)) {
             books.put(res, book);
             book.build(mod, res, external);
         }
         ci.cancel();
+    }
+
+    @Unique
+    private static void patchouliBooks$copyStream(InputStream input, OutputStream output) throws IOException {
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = input.read(buffer)) != -1) {
+            output.write(buffer, 0, bytesRead);
+        }
     }
 }
